bd4e01c2df6b4ed74db95a714a1899459e90e205
William <Hun4104>
2025-07-20 15:19:14 -0500

    Initial Commit

---
+++ Exchange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exchange
+//
+//  Created by William Hunsucker on 7/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
